2c8e201922a9ea8d5dbce0a6bf7e6871a858335f
mac <jossyelmos@yahoo.com>
2023-03-27 22:39:56 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        // console.log(userAuth);
         dispatch(login({
           uid: userAuth.uid,
           email: userAuth.email
